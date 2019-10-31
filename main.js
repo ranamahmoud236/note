6c2e2e886fbe264025dc2af6e589e23f7eba50f4
ranamahmoud236 <ranaimteer@gmail.com>
2019-10-31 22:26:29 +0200

    first part

---
+++ main.js
@@ -0,0 +1,63 @@
+
+dragElement(document.getElementById("note"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+
+
+class note{
+  constructor(note) {
+    this.note = note ;
+    }
+}
+class notes {
+  constructor(){
+    this.notes = JSON.parse(localStorage.getItem('notes')); 
+    if(this.notes == null){
+      this.notes = [];
+    }
+  }
+  addNote(newNote) {
+    this.notes.push(newNote);
+    localStorage.setItem('notes', JSON.stringify(this.notes));
+  }
+}
+let notes = new notes();
+function addNotes() {
+  let note = document.getElementById("note").value;
+
+  let newNote = new note(note, new Date().toTimeString());
+  notes.addNote(newNote);
+}
